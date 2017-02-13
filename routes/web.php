85ac2d81970ccdb29c5a4ed1ae5c8327928824ce
heng_sopheak <hengsopheakbest@gmail.com>
2017-02-13 14:23:34 +0700

    hel

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,9 @@
 Route::get('/', function () {
     return view('layouts.app');
 });
-
-Auth::routes();
 Route::get('/redirect', 'SocialAuthController@redirect');
 Route::get('/callback', 'SocialAuthController@callback');
+
+Auth::routes();
+
 Route::get('/home', 'Controller@index');
