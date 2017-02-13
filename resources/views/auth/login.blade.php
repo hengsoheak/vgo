cdebbf801dad128193642a2e720b9f1a8e7d671d
heng_sopheak <hengsopheakbest@gmail.com>
2017-02-13 14:05:43 +0700

    dd

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -71,4 +71,12 @@
         </div>
     </div>
 </div>
+
+<div
+        class="fb-like"
+        data-share="true"
+        data-width="450"
+        data-show-faces="true">
+</div>
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,24 @@
             'csrfToken' => csrf_token(),
         ]) !!};
     </script>
+    <script>
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '1216717078382739',
+                xfbml      : true,
+                version    : 'v2.8'
+            });
+            FB.AppEvents.logPageView();
+        };
+
+        (function(d, s, id){
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) {return;}
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/sdk.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+    </script>
 </head>
 <body>
     <div id="app">
