a281ef3f09e34da4e14cdd78a85266acb6803292
sopheak01 <h.sopheak01@gmail.com>
2017-02-13 22:38:27 +0700

    /

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -21,7 +21,7 @@ class SocialAuthController extends Controller
     public function callback()
     {
 
-        $providerUser = \Socialite::driver('facebook')->user();
+        $providerUser = Socialite::driver('facebook')->user();
         dd($providerUser);
         // when facebook call us a with token
     }
