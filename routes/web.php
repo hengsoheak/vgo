d76c947bc64f7fa5d9a5ef8da3cd148d5e24f2ca
heng_sopheak <hengsopheakbest@gmail.com>
2017-02-14 12:08:52 +0700

    g

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,21 @@
 Route::get('/', function () {
     return view('layouts.app');
 });
-Route::get('redirect', 'SocialAuthController@redirect');
+Route::get('redirect/{facebook}', [
+    'as'=>'redirectFacebook',
+    'uses'=>'SocialAuthController@redirect'
+])->where('google','[a-z]+');
+
 Route::get('callback', 'SocialAuthController@callback');
+//google
+
+Route::get('redirect/{google}', [
+        'as'=>'redirectGoogle',
+        'uses'=>'SocialAuthController@redirect'
+    ]
+)->where('google','[a-z]+');
+
+Route::get('callback/google', 'SocialAuthController@callback');
 
 Auth::routes();
 
