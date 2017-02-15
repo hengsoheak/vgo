a7b632511edd79e0d04132deb20e580974120d1f
sopheak01 <h.sopheak01@gmail.com>
2017-02-15 22:24:49 +0700

    AA

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -56,7 +56,7 @@
                         </div>
 
                         <div class="form-group">
-                            <div class="col-lg-10 col-md-10 sol-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-0">
+                            <div class="col-lg-10 col-md-10 sol-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-2">
                                 <div class="row">
                                     <div class="panel-heading">
                                         <h4>You can click to login as below Social icone </h4>
@@ -81,7 +81,7 @@
                                             <span class="fa fa-google"></span> Sign in with Google
                                         </a>
                                     </div>
-                                    
+
                                     <!--
                                      <div class="col-xs-4 col-sm-2">
                                          <a class="btn btn-block btn-social btn-flickr" href="{{route('redirectFlickr',['flickr'])}}">
