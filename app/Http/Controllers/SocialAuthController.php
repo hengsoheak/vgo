2fa6ebf08989216fd50fe167a26ca7672513e71f
heng_sopheak <hengsopheakbest@gmail.com>
2017-02-15 12:02:25 +0700

    A

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -47,6 +47,9 @@ class SocialAuthController extends Controller
         //return Socialite::with('twitter')->stateless()->redirect();
 
         $authUser = $this->_findOrCreateUser($providerUser,$providerType);
+        if($providerType== 'google'){
+            dd($authUser);
+        }
 
         Auth::login($authUser, true);
 
