dfdd47325c2ecec44ab2235c8b393486f37ad3f9
heng_sopheak <hengsopheakbest@gmail.com>
2017-02-16 18:04:11 +0700

    A

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\User;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use App\User;
 use Auth;
 
 class Controller extends BaseController
@@ -22,7 +22,7 @@ class Controller extends BaseController
 
     public function index(){
 
-        $this->_data['users']=  User::with(['SocialAccount'])->where('id',33)->first();
+        $this->_data['users']=  User::with('SocialAccount')->where('id',33)->first();
 
         return view('FrontEnd.Home',$this->_data);
     }
