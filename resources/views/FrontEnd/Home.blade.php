7a9abbfa0166aebfadc5922d01d57eeb9cd790b9
Heng Sopheak <hengsopheakbest@gmail.com>
2017-02-16 13:56:40 +0000

    A

--- resources/views/FrontEnd/Home.blade.php
+++ resources/views/FrontEnd/Home.blade.php
@@ -6,18 +6,14 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
 
-                    @foreach($users as $user);
-                            {{dd($user->id)}}
                         <div class="card" style="width: 20rem;">
-                            <img class="card-img-top" src="{{$users->SocialAccount}}" alt="Card image cap">
+                            <img class="card-img-top" src="{{$users->SocialAccount[0]->avatar}}" alt="Card image cap">
                             <div class="card-block">
                                 <h4 class="card-title">Card title</h4>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                                 <a href="#" class="btn btn-primary">Go somewhere</a>
                             </div>
                         </div>
-
-                    @endforeach
 
                 </div>
             </div>
