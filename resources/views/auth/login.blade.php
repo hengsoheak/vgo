3294bde877a836c65286537d8b26cfe6ccdeea62
sopheak01 <h.sopheak01@gmail.com>
2017-02-16 22:51:43 +0700

    A

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -5,7 +5,6 @@
 @endsection
 
 @section('content')
-<div class="container">
     <div class="row">
         <div class="col-md-12 col-md-offset-0">
             <div class="panel panel-default">
@@ -95,7 +94,6 @@
                 </div>
             </div>
         </div>
-    </div>
 </div>
 
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,7 @@
     <link href = "{{asset('bootstrap/css/bootstrap-social.css')}}" rel = "stylesheet">
     <link href = "{{asset('bootstrap/css/docs.css')}}" rel = "stylesheet">
     <link href = "{{asset('font-awesome-4.7.0/css/font-awesome.css')}}" rel = "stylesheet">
+    <link href = "{{asset('css/footer.css')}}" rel = "stylesheet">
 
     <!-- Scripts -->
     <script>
@@ -102,12 +103,89 @@
 
     <div class="wrapper">
         <div class="container">
-            <!--Begin COntent-->
+            <!--Begin Content-->
+
             @yield('content')<!--End Content-->
         </div>
     </div>
 
 </div>
+
+<footer>
+    <div class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+
+                <div class="col-lg-3  col-md-2 col-sm-4 col-xs-6">
+                    <h3> Facebook page </h3>
+                    <ul>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3  col-md-2 col-sm-4 col-xs-6">
+                    <h3> TWITTER </h3>
+                    <ul>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3  col-md-2 col-sm-4 col-xs-6">
+                    <h3> GOOGLE PLUS </h3>
+                    <ul>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                    </ul>
+                </div>
+                <div class="col-lg-3  col-md-2 col-sm-4 col-xs-6">
+                    <h3> LINKEDIN </h3>
+                    <ul>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                        <li> <a href="#"> Lorem Ipsum </a> </li>
+                    </ul>
+                </div>
+                </div>
+
+                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 ">
+                    <div class="row">
+                        <div class="panel-heading">
+
+                        <div class="pannel-body">
+                            <h4><a href="/privancy">privancy</a></h4>
+                            <p>Disclaimer: All content is provided for fun and entertainment purposes only.</p>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-bottom">
+        <div class="container">
+            <p class="pull-left"> Copyright © Camvgo.com 2017. All right reserved. </p>
+            <div class="pull-right">
+                <ul class="nav nav-pills payments">
+                    <li><i class="fa fa-cc-visa"></i></li>
+                    <li><i class="fa fa-cc-mastercard"></i></li>
+                    <li><i class="fa fa-cc-amex"></i></li>
+                    <li><i class="fa fa-cc-paypal"></i></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!--/.footer-bottom-->
+</footer>
 
 <!-- Scripts -->
 <script src = "js/app.js"></script>
