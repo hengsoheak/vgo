19dd907e80e0ed993e4399380eef5a6ea89243cd
sopheak01 <h.sopheak01@gmail.com>
2017-02-16 18:57:48 +0700

    A

--- resources/views/FrontEnd/Home.blade.php
+++ resources/views/FrontEnd/Home.blade.php
@@ -6,9 +6,10 @@
             <div class="col-md-8 col-md-offset-2">
                 <div class="panel panel-default">
 
-
+                        {{dd($users->SocialAccount)}}
+                    
                         <div class="card" style="width: 20rem;">
-                            <img class="card-img-top" src="{{$users->SocialAccount->avatar}}" alt="Card image cap">
+                            <img class="card-img-top" src="{{$users->SocialAccount}}" alt="Card image cap">
                             <div class="card-block">
                                 <h4 class="card-title">Card title</h4>
                                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
