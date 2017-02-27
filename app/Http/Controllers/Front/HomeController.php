be6721122de3e0496980ac5651b8173968e38049
sopheak01 <h.sopheak01@gmail.com>
2017-02-28 01:30:44 +0700

    A

--- app/Http/Controllers/Front/HomeController.php
+++ app/Http/Controllers/Front/HomeController.php
@@ -30,8 +30,8 @@ class HomeController extends FrontController
 
     public function get_cards() {
 
-        $cards = CardType::with(['CardTypeDescription'])
-            ->with(['cards', 'cards.cardDescr'])->get();
+        $cards = CardType::with(['CardTypeDescription'])->with(['cards', 'cards.cardDescr'])->get();
+        
         if(count($cards) > 0) {
             return ['cards'=>$cards];
         }
